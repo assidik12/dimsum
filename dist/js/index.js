a292b4ca41d8117a3f8678b8c38c0dc0eac29aa7
assidik12 <sofi.sidik12@gmail.com>
2024-08-09 21:49:19 +0700

    update patch 2

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,17 +1,18 @@
-// // humberger menu click
-// const humberger = document.querySelector("#humberger");
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  // back to top
+  const toTop = document.querySelector("#to-top");
+  const judul = document.querySelector(".judul");
 
-// const navMenu = document.querySelector("#nav-menu");
-
-// humberger.addEventListener("click", function () {
-//   humberger.classList.toggle("humberger-active");
-//   navMenu.classList.toggle("hidden");
-// });
-
-// // klik di luar humberger
-// window.addEventListener("click", function (e) {
-//   if (e.target != humberger && e.target != navMenu) {
-//     humberger.classList.remove("humberger-active");
-//     navMenu.classList.add("hidden");
-//   }
-// });
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    judul.classList.remove("judul");
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
+};
